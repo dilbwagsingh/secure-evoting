861536604c45015a22e0add4d7f9eb8b7957acee
Dilbwag Singh <dilbwagsingh.che18@iitbhu.ac.in>
2021-03-31 13:37:48 +0530

    Required files are moved inside secure-elections folder.

--- backend_crypto/hash.js
+++
@@ -1,8 +0,0 @@
-var crypto = require('crypto');
-
-module.exports = function(message){
-    this.sha = crypto.createHash('sha256').update(message).digest('hex');
-    this.hash = function(){
-        return this.sha;
-    }
-}
